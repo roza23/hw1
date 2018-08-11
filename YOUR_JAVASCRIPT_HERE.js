2fc7ef488f5912b7a4ac419389634dadbf3f2d26
“Rozanne” <hallo@rozanneottenhoff.nl>
2018-08-11 10:54:53 +0200

    hero object made + game logic function

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,1 +1,29 @@
 // Write your JS here
+
+// An object called hero with 5 propertys with various datat-types(string, bolean, array, number) and the last one equals a nested array
+let hero = {
+    name: 'Sil',
+    heroic: true,
+    inventory: [],
+    health: 10,
+    weapon: {
+        type: '',
+        damage: 2
+    }
+    
+};
+
+// Defined the funtions rest, pickUpItem, equipWeapon
+function rest() {
+    
+}
+
+function pickUpItem() {
+
+}
+
+function equipWeapon() {
+
+}
+
+//hero.health = 0;
