5a24156a81c545fc2698e0a2790504204e525c1a
“Rozanne” <hallo@rozanneottenhoff.nl>
2018-08-11 15:29:20 +0200

    working on seccond function

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,8 +1,9 @@
 // Write your JS here
 
+// An object called hero with 5 keys 
+//with various datat-types(string, bolean, array, number) 
+//and the last one equals a nested array
 
-
-// An object called hero with 5 propertys with various datat-types(string, bolean, array, number) and the last one equals a nested array
 let hero = {
     name: 'Sil',
     heroic: true,
@@ -12,40 +13,55 @@ let hero = {
         type: '',
         damage: 2
     }
-    
 };
+
 
 // Defined the funtions rest, pickUpItem, equipWeapon
 // function rest with no paramerters takes an object ne as an argument named 
 // numberHealth in the numberHealth object the hero value of health will
 // be reassinged to 2. 
-function rest() {
-   let numberHealth = {
-       herohealth: 2 
-    
-    }
+// I was trying to reassign the health hero of the previous object in the object of 
+// function rest but it is not working. 
 
-return numberHealth.herohealth
+
+function rest() {
+// rest takes an object as an argument so made an new object 
+// and then tryed to reassign the value of hero health from the previous global object(hero)
+   let numberHealth = {
+    health2: hero.health = 2 
+    }
+ 
+//   let numberHealth = {
+//   newhealth = hero.health[2] 
+//   hero.health = 2 
+//   }
+
+    return numberHealth // here I return the object numberHealth<-(wich is not working) 
+    const checkRest = document.getElementsByClassName('inn').addEventListener("click"); //calling the  
 }
+
+
 
 function pickUpItem() {
 
     let pickUpHero = {
-
+     weapon: ['name', 'heroic', 'inventory', 'health', 'weapon']; // array stored in object called 
     }
 
+    let seccondObject = {
+
+    }
+    
+    const chekDagger = document.getElementsByClassName('dagger')
 
 }
 
 function equipWeapon() {
+    let pickUpHero = []
 
-    let pickUpHero = {
-
+    if (pickUpHero = 0){
+        console.log()
     }
-
-    if (pickUpHero = true){
-    
-    } 
 }
 
 
